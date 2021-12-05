d27307822732776826775e8a9a759ad1f80e8008
Tobias Udtke <tobias.udtke@gmail.com>
2021-12-05 18:41:24 +0100

    202105: add template and implement parsing.

---
+++ 2021/05_hydrothermal_venture/test_aoc202105.py
@@ -0,0 +1,74 @@
+"""Tests for AoC 5, 2021: Hydrothermal Venture"""
+
+# Standard library imports
+import pathlib
+
+# Third party imports
+import aoc202105
+import pytest
+
+PUZZLE_DIR = pathlib.Path(__file__).parent
+
+
+@pytest.fixture
+def example1() -> aoc202105.InputType:
+    puzzle_input = (PUZZLE_DIR / "example1.txt").read_text().strip()
+    return aoc202105.parse(puzzle_input)
+
+
+@pytest.fixture
+def example2() -> aoc202105.InputType:
+    puzzle_input = (PUZZLE_DIR / "example2.txt").read_text().strip()
+    return aoc202105.parse(puzzle_input)
+
+
+@pytest.fixture
+def puzzle_input() -> aoc202105.InputType:
+    puzzle_input = (PUZZLE_DIR / "input.txt").read_text().strip()
+    return aoc202105.parse(puzzle_input)
+
+
+def test_parse_example1(example1) -> None:
+    """Test that input is parsed properly."""
+    assert example1 == [
+        ((0, 9), (5, 9)),
+        ((8, 0), (0, 8)),
+        ((9, 4), (3, 4)),
+        ((2, 2), (2, 1)),
+        ((7, 0), (7, 4)),
+        ((6, 4), (2, 0)),
+        ((0, 9), (2, 9)),
+        ((3, 4), (1, 4)),
+        ((0, 0), (8, 8)),
+        ((5, 5), (8, 2)),
+    ]
+
+
+@pytest.mark.skip(reason="Not implemented")
+def test_part1_puzzle_input(puzzle_input):
+    """Test part 1 on full input."""
+    assert aoc202105.part1(puzzle_input) == ...
+
+
+@pytest.mark.skip(reason="Not implemented")
+def test_part1_example1(example1) -> None:
+    """Test part 1 on example input"""
+    assert aoc202105.part1(example1) == ...
+
+
+@pytest.mark.skip(reason="Not implemented")
+def test_part2_example1(example1):
+    """Test part 2 on example input"""
+    assert aoc202105.part2(example1) == ...
+
+
+@pytest.mark.skip(reason="Not implemented")
+def test_part2_example2(example2):
+    """Test part 2 on example input"""
+    assert aoc202105.part2(example2) == ...
+
+
+@pytest.mark.skip(reason="Not implemented")
+def test_part2_puzzle_input(puzzle_input):
+    """Test part 2 on example input."""
+    assert aoc202105.part2(puzzle_input) == ...
