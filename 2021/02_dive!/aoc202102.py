d978fa3efd3e566382ac3b0b804d859f3d28ec21
Tobias Udtke <tobias.udtke@gmail.com>
2021-12-05 16:18:01 +0100

    202102: add initialized template.

---
+++ 2021/02_dive!/aoc202102.py
@@ -0,0 +1,36 @@
+"""AoC 2, 2021: Dive!"""
+
+from aocd import get_data
+
+InputType = list[int]
+OutputType = int
+
+
+def parse(puzzle_input: str) -> InputType:
+    """Parse file input."""
+    return puzzle_input.split("\n")
+
+
+def part1(data: InputType) -> OutputType:
+    """Solve part 1."""
+
+
+def part2(data: InputType) -> OutputType:
+    """Solve part 2."""
+
+
+def solve(data: InputType) -> list[str]:
+    """Solve the puzzle for the given input."""
+    solution1 = part1(data)
+    solution2 = part2(data)
+
+    ret = [str(solution1)]
+    if solution2 is not None:
+        ret.append(str(solution2))
+
+    return ret
+
+
+if __name__ == "__main__":
+    data = parse(get_data(year=2021, day=2))
+    print("\n".join(solve(data)))
