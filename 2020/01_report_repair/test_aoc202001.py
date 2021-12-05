7416821860351085139efcf75bb025ef85bf8deb
Tobias Udtke <tobias.udtke@gmail.com>
2021-12-05 23:00:18 +0100

    202001: activate tests.

--- 2020/01_report_repair/test_aoc202001.py
+++ 2020/01_report_repair/test_aoc202001.py
@@ -28,37 +28,26 @@ def puzzle_input() -> aoc202001.InputType:
     return aoc202001.parse(puzzle_input)
 
 
-@pytest.mark.skip(reason="Not implemented")
 def test_parse_example1(example1: aoc202001.InputType) -> None:
     """Test that input is parsed properly."""
-    assert example1 == ...
+    assert example1 == [1721, 979, 366, 299, 675, 1456]
 
 
-@pytest.mark.skip(reason="Not implemented")
 def test_part1_example1(example1: aoc202001.InputType) -> None:
     """Test part 1 on example input"""
-    assert aoc202001.part1(example1) == ...
+    assert aoc202001.part1(example1) == 514579
 
 
-@pytest.mark.skip(reason="Not implemented")
 def test_part1_puzzle_input(puzzle_input: aoc202001.InputType) -> None:
     """Test part 1 on full input."""
-    assert aoc202001.part1(puzzle_input) == ...
+    assert aoc202001.part1(puzzle_input) == 926464
 
 
-@pytest.mark.skip(reason="Not implemented")
 def test_part2_example1(example1: aoc202001.InputType) -> None:
     """Test part 2 on example input"""
-    assert aoc202001.part2(example1) == ...
+    assert aoc202001.part2(example1) == 241861950
 
 
-@pytest.mark.skip(reason="Not implemented")
-def test_part2_example2(example2: aoc202001.InputType) -> None:
-    """Test part 2 on example input"""
-    assert aoc202001.part2(example2) == ...
-
-
-@pytest.mark.skip(reason="Not implemented")
 def test_part2_puzzle_input(puzzle_input: aoc202001.InputType) -> None:
     """Test part 2 on example input."""
-    assert aoc202001.part2(puzzle_input) == ...
+    assert aoc202001.part2(puzzle_input) == 65656536
