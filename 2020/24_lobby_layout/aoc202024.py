e0beb9341fd02b1ad7a266c8c7be0404ad9f1c29
Tobias Udtke <tobias.udtke@gmail.com>
2021-12-06 01:45:17 +0100

    202024: implement part 1 and 2.

--- 2020/24_lobby_layout/aoc202024.py
+++ 2020/24_lobby_layout/aoc202024.py
@@ -1,4 +1,5 @@
 """AoC 24, 2020: Lobby Layout"""
+import itertools
 
 from aocd import get_data
 
@@ -11,12 +12,69 @@ def parse(puzzle_input: str) -> InputType:
     return puzzle_input.split("\n")
 
 
+def turn_tiles(data: InputType) -> set[tuple[int, int]]:
+    black_tiles = set()
+    for line in data:
+        a = b = 0
+        directions = iter(line)
+        for c in directions:
+            if c == "e":
+                a += 1
+            elif c == "w":
+                a -= 1
+            elif c == "n":
+                c = next(directions)
+                if c == "e":
+                    a += 1
+                    b += 1
+                else:
+                    b += 1
+            else:
+                c = next(directions)
+                if c == "e":
+                    b -= 1
+                else:
+                    a -= 1
+                    b -= 1
+        black_tiles.symmetric_difference_update({(a, b)})
+    return black_tiles
+
+
 def part1(data: InputType) -> OutputType:
     """Solve part 1."""
+    return len(turn_tiles(data))
+
+
+def get_neighbours(a: int, b: int) -> list[tuple[int, int]]:
+    return [
+        (a + 1, b), (a - 1, b), (a, b + 1), (a, b - 1), (a + 1, b + 1), (a - 1, b - 1)
+    ]
+
+
+def update_tiles(tiles: set[tuple[int, int]]) -> set[tuple[int, int]]:
+    s = sorted(sum(tiles, start=()))
+    min_, *_, max_ = s
+    # print(s, min_, max_)
+
+    ret = set()
+    for a, b in itertools.product(range(min_ - 1, max_ + 2), repeat=2):
+        n = len(list(filter(lambda x: x in tiles, get_neighbours(a, b))))
+        # print(a, b, n)
+        if (a, b) in tiles:
+            if 1 <= n <= 2:
+                ret.add((a, b))
+        else:
+            if n == 2:
+                ret.add((a, b))
+    return ret
 
 
 def part2(data: InputType) -> OutputType:
     """Solve part 2."""
+    tiles = turn_tiles(data)
+    for _ in range(100):
+        tiles = update_tiles(tiles)
+    return len(tiles)
 
 
 def solve(data: InputType) -> list[str]:
--- 2020/24_lobby_layout/test_aoc202024.py
+++ 2020/24_lobby_layout/test_aoc202024.py
@@ -28,34 +28,54 @@ def puzzle_input() -> aoc202024.InputType:
     return aoc202024.parse(puzzle_input)
 
 
-@pytest.mark.skip(reason="Not implemented")
 def test_parse_example1(example1: aoc202024.InputType) -> None:
     """Test that input is parsed properly."""
-    assert example1 == ...
+    assert example1 == [
+        "sesenwnenenewseeswwswswwnenewsewsw",
+        "neeenesenwnwwswnenewnwwsewnenwseswesw",
+        "seswneswswsenwwnwse",
+        "nwnwneseeswswnenewneswwnewseswneseene",
+        "swweswneswnenwsewnwneneseenw",
+        "eesenwseswswnenwswnwnwsewwnwsene",
+        "sewnenenenesenwsewnenwwwse",
+        "wenwwweseeeweswwwnwwe",
+        "wsweesenenewnwwnwsenewsenwwsesesenwne",
+        "neeswseenwwswnwswswnw",
+        "nenwswwsewswnenenewsenwsenwnesesenew",
+        "enewnwewneswsewnwswenweswnenwsenwsw",
+        "sweneswneswneneenwnewenewwneswswnese",
+        "swwesenesewenwneswnwwneseswwne",
+        "enesenwswwswneneswsenwnewswseenwsese",
+        "wnwnesenesenenwwnenwsewesewsesesew",
+        "nenewswnwewswnenesenwnesewesw",
+        "eneswnwswnwsenenwnwnwwseeswneewsenese",
+        "neswnwewnwnwseenwseesewsenwsweewe",
+        "wseweeenwnesenwwwswnew",
+    ]
 
 
-@pytest.mark.skip(reason="Not implemented")
 def test_part1_example1(example1: aoc202024.InputType) -> None:
     """Test part 1 on example input"""
-    assert aoc202024.part1(example1) == ...
+    assert aoc202024.part1(example1) == 10
 
 
-@pytest.mark.skip(reason="Not implemented")
 def test_part1_puzzle_input(puzzle_input: aoc202024.InputType) -> None:
     """Test part 1 on full input."""
-    assert aoc202024.part1(puzzle_input) == ...
+    assert aoc202024.part1(puzzle_input) == 289
 
 
-@pytest.mark.skip(reason="Not implemented")
 def test_part2_example1(example1: aoc202024.InputType) -> None:
     """Test part 2 on example input"""
-    assert aoc202024.part2(example1) == ...
+    assert aoc202024.part2(example1) == 2208
 
 
-@pytest.mark.skip(reason="Not implemented")
-def test_part2_example2(example2: aoc202024.InputType) -> None:
-    """Test part 2 on example input"""
-    assert aoc202024.part2(example2) == ...
+def test_neighbours() -> None:
+    assert aoc202024.get_neighbours(0, 0) == [(1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (-1, -1)]
+    assert aoc202024.get_neighbours(1, 0) == [(2, 0), (0, 0), (1, 1), (1, -1), (2, 1), (0, -1)]
+
+
+def test_update() -> None:
+    assert aoc202024.update_tiles({(0, 0), (1, 0)}) == {(0, 0), (1, 0), (1, 1), (0, -1)}
 
 
 @pytest.mark.skip(reason="Not implemented")
