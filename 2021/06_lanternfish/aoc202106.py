71cf0ab9d2157272b351d0aa8ff04d44e47c1d06
Tobias Udtke <tobias.udtke@gmail.com>
2021-12-06 15:46:17 +0100

    202106: Implement part 1 and 2.

--- 2021/06_lanternfish/aoc202106.py
+++ 2021/06_lanternfish/aoc202106.py
@@ -1,4 +1,5 @@
 """AoC 6, 2021: Lanternfish"""
+from collections import Counter
 
 from aocd import get_data
 
@@ -11,12 +12,30 @@ def parse(puzzle_input: str) -> InputType:
     return list(map(int, puzzle_input.split(",")))
 
 
+def model_lanternfish(data: InputType, num_days: int) -> int:
+    c = Counter(data)
+    for _ in range(num_days):
+        temp = c[0]
+        c[0] = c[1]
+        c[1] = c[2]
+        c[2] = c[3]
+        c[3] = c[4]
+        c[4] = c[5]
+        c[5] = c[6]
+        c[6] = c[7] + temp
+        c[7] = c[8]
+        c[8] = temp
+    return sum(c.values())
+
+
 def part1(data: InputType) -> OutputType:
     """Solve part 1."""
+    return model_lanternfish(data, 80)
 
 
 def part2(data: InputType) -> OutputType:
     """Solve part 2."""
+    return model_lanternfish(data, 256)
 
 
 def solve(data: InputType) -> list[str]:
--- 2021/06_lanternfish/test_aoc202106.py
+++ 2021/06_lanternfish/test_aoc202106.py
@@ -28,37 +28,26 @@ def puzzle_input() -> aoc202106.InputType:
     return aoc202106.parse(puzzle_input)
 
 
-@pytest.mark.skip(reason="Not implemented")
 def test_parse_example1(example1: aoc202106.InputType) -> None:
     """Test that input is parsed properly."""
-    assert example1 == ...
+    assert example1 == [3, 4, 3, 1, 2]
 
 
-@pytest.mark.skip(reason="Not implemented")
 def test_part1_example1(example1: aoc202106.InputType) -> None:
     """Test part 1 on example input"""
-    assert aoc202106.part1(example1) == ...
+    assert aoc202106.part1(example1) == 5934
 
 
-@pytest.mark.skip(reason="Not implemented")
 def test_part1_puzzle_input(puzzle_input: aoc202106.InputType) -> None:
     """Test part 1 on full input."""
-    assert aoc202106.part1(puzzle_input) == ...
+    assert aoc202106.part1(puzzle_input) == 352872
 
 
-@pytest.mark.skip(reason="Not implemented")
 def test_part2_example1(example1: aoc202106.InputType) -> None:
     """Test part 2 on example input"""
-    assert aoc202106.part2(example1) == ...
+    assert aoc202106.part2(example1) == 26984457539
 
 
-@pytest.mark.skip(reason="Not implemented")
-def test_part2_example2(example2: aoc202106.InputType) -> None:
-    """Test part 2 on example input"""
-    assert aoc202106.part2(example2) == ...
-
-
-@pytest.mark.skip(reason="Not implemented")
 def test_part2_puzzle_input(puzzle_input: aoc202106.InputType) -> None:
     """Test part 2 on example input."""
-    assert aoc202106.part2(puzzle_input) == ...
+    assert aoc202106.part2(puzzle_input) == 1604361182149
