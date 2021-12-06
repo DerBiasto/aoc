423dcf5aa01e2b362833b7458182afd37715a7be
Tobias Udtke <tobias.udtke@gmail.com>
2021-12-06 02:29:19 +0100

    202008: add initial template and parsing.

---
+++ 2020/08_handheld_halting/aoc202008.py
@@ -0,0 +1,39 @@
+"""AoC 8, 2020: Handheld Halting"""
+
+from aocd import get_data
+
+InputType = list[tuple[str, int]]
+OutputType = int
+
+
+def parse(puzzle_input: str) -> InputType:
+    """Parse file input."""
+    return [
+        (line.split()[0], int(line.split()[1]))
+        for line in puzzle_input.split("\n")
+    ]
+
+
+def part1(data: InputType) -> OutputType:
+    """Solve part 1."""
+
+
+def part2(data: InputType) -> OutputType:
+    """Solve part 2."""
+
+
+def solve(data: InputType) -> list[str]:
+    """Solve the puzzle for the given input."""
+    solution1 = part1(data)
+    solution2 = part2(data)
+
+    ret = [str(solution1)]
+    if solution2 is not None:
+        ret.append(str(solution2))
+
+    return ret
+
+
+if __name__ == "__main__":
+    data = parse(get_data(year=2020, day=8))
+    print("\n".join(solve(data)))
---
+++ 2020/08_handheld_halting/test_aoc202008.py
@@ -0,0 +1,73 @@
+"""Tests for AoC 8, 2020: Handheld Halting"""
+
+# Standard library imports
+import pathlib
+
+# Third party imports
+import aoc202008
+import pytest
+
+PUZZLE_DIR = pathlib.Path(__file__).parent
+
+
+@pytest.fixture
+def example1() -> aoc202008.InputType:
+    puzzle_input = (PUZZLE_DIR / "example1.txt").read_text().strip()
+    return aoc202008.parse(puzzle_input)
+
+
+@pytest.fixture
+def example2() -> aoc202008.InputType:
+    puzzle_input = (PUZZLE_DIR / "example2.txt").read_text().strip()
+    return aoc202008.parse(puzzle_input)
+
+
+@pytest.fixture
+def puzzle_input() -> aoc202008.InputType:
+    puzzle_input = (PUZZLE_DIR / "input.txt").read_text().strip()
+    return aoc202008.parse(puzzle_input)
+
+
+def test_parse_example1(example1: aoc202008.InputType) -> None:
+    """Test that input is parsed properly."""
+    assert example1 == [
+        ("nop", 0),
+        ("acc", 1),
+        ("jmp", 4),
+        ("acc", 3),
+        ("jmp", -3),
+        ("acc", -99),
+        ("acc", 1),
+        ("jmp", -4),
+        ("acc", 6),
+    ]
+
+
+@pytest.mark.skip(reason="Not implemented")
+def test_part1_example1(example1: aoc202008.InputType) -> None:
+    """Test part 1 on example input"""
+    assert aoc202008.part1(example1) == ...
+
+
+@pytest.mark.skip(reason="Not implemented")
+def test_part1_puzzle_input(puzzle_input: aoc202008.InputType) -> None:
+    """Test part 1 on full input."""
+    assert aoc202008.part1(puzzle_input) == ...
+
+
+@pytest.mark.skip(reason="Not implemented")
+def test_part2_example1(example1: aoc202008.InputType) -> None:
+    """Test part 2 on example input"""
+    assert aoc202008.part2(example1) == ...
+
+
+@pytest.mark.skip(reason="Not implemented")
+def test_part2_example2(example2: aoc202008.InputType) -> None:
+    """Test part 2 on example input"""
+    assert aoc202008.part2(example2) == ...
+
+
+@pytest.mark.skip(reason="Not implemented")
+def test_part2_puzzle_input(puzzle_input: aoc202008.InputType) -> None:
+    """Test part 2 on example input."""
+    assert aoc202008.part2(puzzle_input) == ...
