98624ec8aab5da6b9021c2e37501943ba4d79dae
Tobias Udtke <tobias.udtke@gmail.com>
2021-12-06 04:40:38 +0100

    202011: add helper class and implement part 1 and 2 (badly).

--- 2020/11_seating_system/aoc202011.py
+++ 2020/11_seating_system/aoc202011.py
@@ -1,25 +1,99 @@
 """AoC 11, 2020: Seating System"""
+import copy
+import itertools
 
 from aocd import get_data
 
-InputType = list[str]
+
+class Grid:
+    occupied = "#"
+    empty = "L"
+    floor = "."
+
+    def __init__(self, grid: list[list[str]]):
+        self._grid = grid
+        self.n = len(grid)
+        self.m = len(grid[0])
+
+    def __getitem__(self, key: int) -> list[str]:
+        return self._grid[key]
+
+    def num_neighbours(self, x: int, y: int, part2: bool) -> int:
+        ret = 0
+        if not part2:
+            for x, y in [(x - 1, y - 1), (x - 1, y), (x - 1, y + 1), (x, y - 1),
+                         (x, y + 1), (x + 1, y - 1), (x + 1, y), (x + 1, y + 1)]:
+                if 0 <= x < self.n and 0 <= y < self.m:
+                    ret += self._grid[x][y] == self.occupied
+        else:
+            for dx, dy in itertools.product((-1, 0, 1), repeat=2):
+                x_ = x
+                y_ = y
+                if dx == dy == 0:
+                    continue
+                while True:
+                    if 0 <= x_ + dx < self.n and 0 <= y_ + dy < self.m:
+                        x_ += dx
+                        y_ += dy
+                        if self[x_][y_] == self.occupied:
+                            ret += 1
+                            break
+                        elif self[x_][y_] == self.empty:
+                            break
+                    else:
+                        break
+        return ret
+
+    def update(self, part2: bool) -> int:
+        ret = 0
+        new_grid = []
+        for x in range(self.n):
+            new_grid.append([''] * self.m)
+            for y in range(self.m):
+                new_grid[x][y] = self[x][y]
+                if self[x][y] == self.empty:
+                    if not self.num_neighbours(x, y, part2):
+                        new_grid[x][y] = self.occupied
+                        ret += 1
+                elif self[x][y] == self.occupied:
+                    if self.num_neighbours(x, y, part2) >= (4 + part2):
+                        new_grid[x][y] = self.empty
+                        ret += 1
+        self._grid = new_grid
+        return ret
+
+    def num_occupied(self) -> int:
+        return sum(1 for line in self._grid for c in line if c == self.occupied)
+
+    def __str__(self) -> str:
+        return "\n".join("".join(line) for line in self._grid)
+
+
+InputType = Grid
 OutputType = int
 
 
 def parse(puzzle_input: str) -> InputType:
     """Parse file input."""
-    return [
-        line
+    return Grid([
+        list(line)
         for line in puzzle_input.split("\n")
-    ]
+    ])
 
 
 def part1(data: InputType) -> OutputType:
     """Solve part 1."""
+    data = copy.deepcopy(data)
+    while data.update(part2=False):
+        pass
+    return data.num_occupied()
 
 
 def part2(data: InputType) -> OutputType:
     """Solve part 2."""
+    while data.update(part2=True):
+        pass
+    return data.num_occupied()
 
 
 def solve(data: InputType) -> list[str]:
--- 2020/11_seating_system/test_aoc202011.py
+++ 2020/11_seating_system/test_aoc202011.py
@@ -30,36 +30,33 @@ def puzzle_input() -> aoc202011.InputType:
 
 def test_parse_example1(example1: aoc202011.InputType) -> None:
     """Test that input is parsed properly."""
-    assert example1 == [
-        "L.LL.LL.LL",
-        "LLLLLLL.LL",
-        "L.L.L..L..",
-        "LLLL.LL.LL",
-        "L.LL.LL.LL",
-        "L.LLLLL.LL",
-        "..L.L.....",
-        "LLLLLLLLLL",
-        "L.LLLLLL.L",
-        "L.LLLLL.LL",
+    assert example1._grid == [
+        list("L.LL.LL.LL"),
+        list("LLLLLLL.LL"),
+        list("L.L.L..L.."),
+        list("LLLL.LL.LL"),
+        list("L.LL.LL.LL"),
+        list("L.LLLLL.LL"),
+        list("..L.L....."),
+        list("LLLLLLLLLL"),
+        list("L.LLLLLL.L"),
+        list("L.LLLLL.LL"),
     ]
 
 
-@pytest.mark.skip(reason="Not implemented")
 def test_part1_example1(example1: aoc202011.InputType) -> None:
     """Test part 1 on example input"""
-    assert aoc202011.part1(example1) == ...
+    assert aoc202011.part1(example1) == 37
 
 
-@pytest.mark.skip(reason="Not implemented")
 def test_part1_puzzle_input(puzzle_input: aoc202011.InputType) -> None:
     """Test part 1 on full input."""
-    assert aoc202011.part1(puzzle_input) == ...
+    assert aoc202011.part1(puzzle_input) == 2361
 
 
-@pytest.mark.skip(reason="Not implemented")
 def test_part2_example1(example1: aoc202011.InputType) -> None:
     """Test part 2 on example input"""
-    assert aoc202011.part2(example1) == ...
+    assert aoc202011.part2(example1) == 26
 
 
 @pytest.mark.skip(reason="Not implemented")
@@ -68,7 +65,6 @@ def test_part2_example2(example2: aoc202011.InputType) -> None:
     assert aoc202011.part2(example2) == ...
 
 
-@pytest.mark.skip(reason="Not implemented")
 def test_part2_puzzle_input(puzzle_input: aoc202011.InputType) -> None:
     """Test part 2 on example input."""
-    assert aoc202011.part2(puzzle_input) == ...
+    assert aoc202011.part2(puzzle_input) == 2315
