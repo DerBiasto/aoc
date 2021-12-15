8cb11b261a80ab7000e4d43b936558c50014db9b
Tobias Udtke <tobias.udtke@gmail.com>
2021-12-15 09:02:19 +0100

    202010: Add large example.

--- 2021/10_syntax_scoring/test_aoc202110.py
+++ 2021/10_syntax_scoring/test_aoc202110.py
@@ -23,6 +23,12 @@ def example2() -> aoc202110.InputType:
 
 
 @pytest.fixture
+def example3() -> aoc202110.InputType:
+    puzzle_input = (PUZZLE_DIR / "example3.txt").read_text().strip()
+    return aoc202110.parse(puzzle_input)
+
+
+@pytest.fixture
 def puzzle_input() -> aoc202110.InputType:
     puzzle_input = (PUZZLE_DIR / "input.txt").read_text().strip()
     return aoc202110.parse(puzzle_input)
@@ -49,6 +55,11 @@ def test_part1_example1(example1: aoc202110.InputType) -> None:
     assert aoc202110.part1(example1) == 26397
 
 
+def test_part1_example2(example2: aoc202110.InputType) -> None:
+    """Test part 2 on example input"""
+    assert aoc202110.part1(example2) == 32852823
+
+
 def test_part1_puzzle_input(puzzle_input: aoc202110.InputType) -> None:
     """Test part 1 on full input."""
     assert aoc202110.part1(puzzle_input) == 311895
@@ -59,10 +70,14 @@ def test_part2_example1(example1: aoc202110.InputType) -> None:
     assert aoc202110.part2(example1) == 288957
 
 
-@pytest.mark.skip(reason="Not implemented")
 def test_part2_example2(example2: aoc202110.InputType) -> None:
     """Test part 2 on example input"""
-    assert aoc202110.part2(example2) == ...
+    assert aoc202110.part2(example2) == 105721020868542718163333508098695965856824383141538359
+
+
+def test_part2_example3(example3: aoc202110.InputType) -> None:
+    """Test part 2 on example input"""
+    assert aoc202110.part2(example3) == 4147221136278686118851251078439942241593032865422495540589075652007224217627158672406548941332753773231145137591607755327259751330408129749234363801432056312168742919300272994666758197001661058825916005929107429822715605939942443473451119156732403958992327362236310858256528168096490885509843074970456349764872605113646021900154182133457634302249036648560726381663678169920580181158091185711536415744289810493158025390014526746339533063857301193312008845372928578055750021478792579394195467723166421532710570596861139275905662620238
 
 
 def test_part2_puzzle_input(puzzle_input: aoc202110.InputType) -> None:
